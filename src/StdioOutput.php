504fdfd7c3dbdfe82d2f20f1aa3f5a55408e4c93
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-05-17 07:59:22 +0200

    Extend Output base class

--- src/StdioOutput.php
+++ src/StdioOutput.php
@@ -3,10 +3,9 @@
 namespace WyriHaximus\React\Symfony\Console;
 
 use Clue\React\Stdio\Stdio;
-use Symfony\Component\Console\Formatter\OutputFormatterInterface;
-use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Output\Output;
 
-final class StdioOutput implements OutputInterface
+final class StdioOutput extends Output
 {
     /**
      * @var Stdio
@@ -19,72 +18,12 @@ final class StdioOutput implements OutputInterface
     public function __construct(Stdio $stdio)
     {
         $this->stdio = $stdio;
+        parent::__construct();
     }
 
-    public function write($messages, $newline = false, $options = 0)
+    public function doWrite($message, $newline = false)
     {
-        if (!is_array($messages)) {
-            $messages = [$messages];
-        }
-
         $lineEnding = $newline ? PHP_EOL : '';
-        foreach ($messages as $message) {
-            $this->stdio->write($message . $lineEnding);
-        }
-    }
-
-    public function writeln($messages, $options = 0)
-    {
-        $this->write($messages, true, $options);
-    }
-
-    public function setVerbosity($level)
-    {
-        // TODO: Implement setVerbosity() method.
-    }
-
-    public function getVerbosity()
-    {
-        // TODO: Implement getVerbosity() method.
-    }
-
-    public function isQuiet()
-    {
-        // TODO: Implement isQuiet() method.
-    }
-
-    public function isVerbose()
-    {
-        // TODO: Implement isVerbose() method.
-    }
-
-    public function isVeryVerbose()
-    {
-        // TODO: Implement isVeryVerbose() method.
-    }
-
-    public function isDebug()
-    {
-        // TODO: Implement isDebug() method.
-    }
-
-    public function setDecorated($decorated)
-    {
-        // TODO: Implement setDecorated() method.
-    }
-
-    public function isDecorated()
-    {
-        // TODO: Implement isDecorated() method.
-    }
-
-    public function setFormatter(OutputFormatterInterface $formatter)
-    {
-        // TODO: Implement setFormatter() method.
-    }
-
-    public function getFormatter()
-    {
-        // TODO: Implement getFormatter() method.
+        $this->stdio->write($message . $lineEnding);
     }
 }
